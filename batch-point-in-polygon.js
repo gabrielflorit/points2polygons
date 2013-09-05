66f03d9119011deda0ac39ce3e8a77299823e349
Gabriel Florit <gabrielflorit@gmail.com>
2013-09-05 15:15:35 -0400

    added support for multi-polygons

--- batch-point-in-polygon.js
+++ batch-point-in-polygon.js
@@ -1,4 +1,3 @@
-var _ = require('lodash');
 var inside = require('point-in-polygon');
 
 var batch = function(polygons, points, showProgress) {
@@ -6,28 +5,59 @@ var batch = function(polygons, points, showProgress) {
 	var insidePointsCount = 0;
 	var orphans = [];
 
-	var start = Date.now();
-
 	// for each point,
-	_.each(points.features, function(point, i, array) {
+	points.features.forEach(function(point, i, array) {
 
 		var found = false;
 
 		// for each polygon,
-		_.each(polygons.features, function(polygon) {
+		polygons.features.forEach(function(polygon) {
 
-			// if the point is inside this polygon, add it
-			if (!found && inside(point.geometry.coordinates, polygon.geometry.coordinates[0])) {
+			var geometryType = polygon.geometry.type;
 
-				found = true;
+			if (geometryType === 'Polygon') {
 
-				if (!polygon.properties.points) {
-					polygon.properties.points = [];
+				// only search if not found
+				if (!found) {
+
+					if (inside(point.geometry.coordinates, polygon.geometry.coordinates[0])) {
+
+						found = true;
+
+						if (!polygon.properties.points) {
+							polygon.properties.points = [];
+						}
+
+						polygon.properties.points.push(point);
+						insidePointsCount++;
+					}
+
 				}
+			}
 
-				polygon.properties.points.push(point);
-				insidePointsCount++;
+			if (geometryType === 'MultiPolygon') {
+	
+				// for each coordinates
+				polygon.geometry.coordinates.forEach(function(coordinates) {
 
+					// only search if not found
+					if (!found) {
+
+						if (inside(point.geometry.coordinates, coordinates[0])) {
+
+							found = true;
+
+							if (!polygon.properties.points) {
+								polygon.properties.points = [];
+							}
+
+							polygon.properties.points.push(point);
+							insidePointsCount++;
+						}
+
+					}
+
+				});
 			}
 
 		});
