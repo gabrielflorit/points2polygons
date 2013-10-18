bc60f54e30534631ad18591d215be91c2e46141d
Gabriel Florit <gabrielflorit@gmail.com>
2013-10-18 12:28:12 -0400

    started adding tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,64 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var polygon = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+			"type": "Polygon",
+			"coordinates": [
+					[
+						[0,0],
+						[3,0],
+						[3,3],
+						[0,3],
+						[0,2],
+						[2,2],
+						[2,1],
+						[0,1],
+						[0,0]
+					]
+				]
+			}
+		}
+	]
+};
+
+var outsidePoint = {
+	"type": "FeatureCollection",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [1.5,1.5]
+			}
+		}
+	]
+};
+
+var assert = require("assert");
+var _ = require('lodash');
+var pip = require('../batch-point-in-polygon.js');
+
+describe('#batch-point-in-polygon()', function() {
+	it('should not place outside point inside the polygon', function() {
+
+		var result = pip.batch(polygon, outsidePoint);
+		assert.deepEqual(result.polygons, polygon);
+		assert.deepEqual(result.orphans, outsidePoint.features);
+
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
